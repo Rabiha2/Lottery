5f6ecfb9c5f78fe403ac02fbeed94b346728b0f1
rabiha <rojidrabiha@gmail.com>
2024-06-25 23:56:35 +0400

    Initial commit

--- contracts/Proposal.sol
+++ contracts/Proposal.sol
@@ -2,7 +2,10 @@
 pragma solidity ^0.8.18;
 
 contract ProposalContract {
+    address owner;
+
     uint256 private counter;
+
     struct Proposal {
         string title; // Title of the proposal
         string description; // Description of the proposal
@@ -16,9 +19,118 @@ contract ProposalContract {
 
     mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
 
-    function create(string calldata _title,string calldata _description, uint256 _total_vote_to_end) external {
+    address[] private voted_addresses;
+
+    constructor() {
+        owner = msg.sender;
+        voted_addresses.push(msg.sender);
+    }
+
+    function create(
+        string calldata _title,
+        string calldata _description,
+        uint256 _total_vote_to_end
+    ) external onlyOwner {
         counter += 1;
-        proposal_history[counter] = Proposal(_title,_description, 0, 0, 0, _total_vote_to_end, false, true);
-}
+        proposal_history[counter] = Proposal(
+            _title,
+            _description,
+            0,
+            0,
+            0,
+            _total_vote_to_end,
+            false,
+            true
+        );
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier active() {
+        require(proposal_history[counter].is_active == true);
+        _;
+    }
+
+    modifier newVoter(address _address) {
+        require(!isVoted(_address), "Address has already voted");
+        _;
+    }
+
+    function setOwner(address new_owner) external onlyOwner {
+        owner = new_owner;
+    }
+
+    function isVoted(address _address) public view returns (bool) {
+    for (uint i = 0; i < voted_addresses.length; i++) {
+        if (voted_addresses[i] == _address) {
+            return true;
+        }
+    }
+    return false;
 }
 
+    function vote(uint8 choice) external newVoter(msg.sender) active {
+        //First part
+        Proposal storage proposal = proposal_history[counter];
+        uint256 total_vote = proposal.approve + proposal.reject + proposal.pass;
+
+        voted_addresses.push(msg.sender);
+
+        // Second part
+        if (choice == 1) {
+            proposal.approve += 1;
+            proposal.current_state = calculateCurrentState();
+        } else if (choice == 2) {
+            proposal.reject += 1;
+            proposal.current_state = calculateCurrentState();
+        } else if (choice == 0) {
+            proposal.pass += 1;
+            proposal.current_state = calculateCurrentState();
+        }
+
+        // Third part
+        if (
+            (proposal.total_vote_to_end - total_vote == 1) &&
+            (choice == 1 || choice == 2 || choice == 0)
+        ) {
+            proposal.is_active = false;
+            voted_addresses = [owner];
+        }
+    }
+
+    function calculateCurrentState() private view returns(bool) {
+    Proposal storage proposal = proposal_history[counter];
+
+    uint256 approve = proposal.approve;
+    uint256 reject = proposal.reject;
+    uint256 pass = proposal.pass;
+        
+    if (proposal.pass %2 == 1) {
+        pass += 1;
+    }
+
+    pass = pass / 2;
+
+    if (approve > reject + pass) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function teminateProposal() external onlyOwner active {
+    proposal_history[counter].is_active = false;
+}
+
+function getCurrentProposal() external view returns(Proposal memory) {
+    return proposal_history[counter];
+}
+
+function getProposal(uint256 number) external view returns(Proposal memory) {
+    return proposal_history[number];
+}
+
+}
